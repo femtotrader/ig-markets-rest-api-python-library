d349c3fe922031c079a38d2c2386aaac75548b38
femtotrader <femto.trader@gmail.com>
2015-01-19 20:53:19 +0100

    Tests with cached session

--- tests/test_ig_service.py
+++ tests/test_ig_service.py
@@ -14,8 +14,9 @@ import pandas as pd
 import pprint
 import os
 import datetime
+import requests
 import requests_cache
-#from datetime import datetime, timedelta
+from datetime import datetime, timedelta
 import six
 import time
 
@@ -33,152 +34,158 @@ export IG_SERVICE_ACC_NUMBER=""
 
 
 def test_ig_service():
+
     DELAY = 15
     def wait(delay):
         print("Wait %s s to avoid error.public-api.exceeded-account-allowance" % delay)
         time.sleep(delay)
 
-    pp = pprint.PrettyPrinter(indent=4)
+    session_cached = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=timedelta(hours=1))
+    session_not_cached = requests.Session()
 
-    assert(isinstance(trading_ig.__version__, six.string_types))
+    for session in [session_cached, session_cached, session_not_cached]:
 
-    config = ConfigEnvVar("IG_SERVICE")
-    ig_service = IGService(config.username, config.password, config.api_key, config.acc_type)
-    ig_service.create_session()
+        pp = pprint.PrettyPrinter(indent=4)
 
-    print("fetch_accounts")
-    response = ig_service.fetch_accounts()
-    print(response)
-    #assert(response['balance'][0]['available']>0)
-    assert(response['balance'][0]>0)
+        assert(isinstance(trading_ig.__version__, six.string_types))
 
-    print("")
+        config = ConfigEnvVar("IG_SERVICE")
+        ig_service = IGService(config.username, config.password, config.api_key, config.acc_type)
+        ig_service.create_session()
 
-    print("fetch_account_activity_by_period")
-    response = ig_service.fetch_account_activity_by_period(10000)
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
+        print("fetch_accounts")
+        response = ig_service.fetch_accounts()
+        print(response)
+        #assert(response['balance'][0]['available']>0)
+        assert(response['balance'][0]>0)
 
-    print("")
+        print("")
 
-    print("fetch_account_activity_by_period")
-    response = ig_service.fetch_account_activity_by_period(10000)
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
+        print("fetch_account_activity_by_period")
+        response = ig_service.fetch_account_activity_by_period(10000)
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
 
-    print("")
+        print("")
 
-    print("fetch_transaction_history_by_type_and_period")
-    response = ig_service.fetch_transaction_history_by_type_and_period(10000, "ALL")
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
+        print("fetch_account_activity_by_period")
+        response = ig_service.fetch_account_activity_by_period(10000)
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
 
-    wait(DELAY)
-    print("")
+        print("")
 
-    print("fetch_open_positions")
-    response = ig_service.fetch_open_positions()
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
+        print("fetch_transaction_history_by_type_and_period")
+        response = ig_service.fetch_transaction_history_by_type_and_period(10000, "ALL")
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
 
-    print("")
+        wait(DELAY)
+        print("")
 
-    print("fetch_working_orders")
-    response = ig_service.fetch_working_orders()
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
+        print("fetch_open_positions")
+        response = ig_service.fetch_open_positions()
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
 
-    print("")
+        print("")
 
-    print("fetch_top_level_navigation_nodes")
-    response = ig_service.fetch_top_level_navigation_nodes()
-    print(response) # dict with nodes and markets
-    assert(isinstance(response, dict))
-    market_id = response['nodes']['id'].iloc[0]
+        print("fetch_working_orders")
+        response = ig_service.fetch_working_orders()
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
 
-    print("")
+        print("")
 
-    print("fetch_client_sentiment_by_instrument")
-    response = ig_service.fetch_client_sentiment_by_instrument(market_id)
-    print(response)
-    assert(isinstance(response, dict))
+        print("fetch_top_level_navigation_nodes")
+        response = ig_service.fetch_top_level_navigation_nodes()
+        print(response) # dict with nodes and markets
+        assert(isinstance(response, dict))
+        market_id = response['nodes']['id'].iloc[0]
 
-    print("")
+        print("")
 
-    print("fetch_related_client_sentiment_by_instrument")
-    response = ig_service.fetch_related_client_sentiment_by_instrument(market_id)
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
+        print("fetch_client_sentiment_by_instrument")
+        response = ig_service.fetch_client_sentiment_by_instrument(market_id)
+        print(response)
+        assert(isinstance(response, dict))
 
-    print("")
+        print("")
 
-    print("fetch_sub_nodes_by_node")
-    node = market_id #?
-    response = ig_service.fetch_sub_nodes_by_node(node)
-    print(response)
-    assert(isinstance(response['markets'], pd.DataFrame))
-    assert(isinstance(response['nodes'], pd.DataFrame))
+        print("fetch_related_client_sentiment_by_instrument")
+        response = ig_service.fetch_related_client_sentiment_by_instrument(market_id)
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
 
-    print("")
-    wait(DELAY)
+        print("")
 
-    print("fetch_all_watchlists")
-    response = ig_service.fetch_all_watchlists()
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
-    watchlist_id = response['id'].iloc[0] # u'Popular Markets'
-    #epic = 
+        print("fetch_sub_nodes_by_node")
+        node = market_id #?
+        response = ig_service.fetch_sub_nodes_by_node(node)
+        print(response)
+        assert(isinstance(response['markets'], pd.DataFrame))
+        assert(isinstance(response['nodes'], pd.DataFrame))
 
-    print("")
+        print("")
+        wait(DELAY)
 
-    print("fetch_watchlist_markets")
-    response = ig_service.fetch_watchlist_markets(watchlist_id)
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
-    epic = response['epic'].iloc[0] # epic = 'CS.D.EURUSD.MINI.IP' # epic = u'IX.D.CAC.IDF.IP'
+        print("fetch_all_watchlists")
+        response = ig_service.fetch_all_watchlists()
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
+        watchlist_id = response['id'].iloc[0] # u'Popular Markets'
+        #epic = 
 
-    print("")
+        print("")
 
-    print("fetch_market_by_epic")
-    response = ig_service.fetch_market_by_epic(epic)
-    print(response)
-    #pp.pprint(response)
-    assert(isinstance(response, dict))
+        print("fetch_watchlist_markets")
+        response = ig_service.fetch_watchlist_markets(watchlist_id)
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
+        epic = response['epic'].iloc[0] # epic = 'CS.D.EURUSD.MINI.IP' # epic = u'IX.D.CAC.IDF.IP'
 
-    print("")
+        print("")
 
-    print("search_markets")
-    search_term = 'EURUSD'
-    response = ig_service.search_markets(search_term)
-    print(response)
-    assert(isinstance(response, pd.DataFrame))
+        print("fetch_market_by_epic")
+        response = ig_service.fetch_market_by_epic(epic)
+        print(response)
+        #pp.pprint(response)
+        assert(isinstance(response, dict))
 
-    print("")
+        print("")
 
-    print("fetch_historical_prices_by_epic_and_num_points")
+        print("search_markets")
+        search_term = 'EURUSD'
+        response = ig_service.search_markets(search_term)
+        print(response)
+        assert(isinstance(response, pd.DataFrame))
 
-    #epic = 'CS.D.EURUSD.MINI.IP'
-    #resolution = 'HOUR' # MINUTE, MINUTE_2, MINUTE_3, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR, HOUR_2, HOUR_3, HOUR_4, DAY, WEEK, MONTH
-    resolution = 'H' # http://pandas.pydata.org/pandas-docs/stable/timeseries.html#dateoffset-objects
-    num_points = 10
-    response = ig_service.fetch_historical_prices_by_epic_and_num_points(epic, resolution, num_points)
-    print(response)
-    #print(response['prices']['price'])
-    #print(response['prices']['price']['ask'])
-    #print(response['prices']['volume'])
-    assert(isinstance(response['allowance'], dict))
-    #assert(isinstance(response['prices']['volume'], pd.Series))
-    #assert(isinstance(response['prices']['price'], pd.Panel))
-    assert(isinstance(response['prices'], pd.DataFrame))
+        print("")
 
-    print("")
+        print("fetch_historical_prices_by_epic_and_num_points")
 
-    print("fetch_historical_prices_by_epic_and_date_range")
-    start_date = datetime.datetime(2014, 12, 15)
-    end_date = datetime.datetime(2014, 12, 20)
-    response = ig_service.fetch_historical_prices_by_epic_and_date_range(epic, resolution, start_date, end_date)
-    print(response)
-    assert(isinstance(response['allowance'], dict))
-    #assert(isinstance(response['prices']['volume'], pd.Series))
-    #assert(isinstance(response['prices']['price'], pd.Panel))
-    assert(isinstance(response['prices'], pd.DataFrame))
+        #epic = 'CS.D.EURUSD.MINI.IP'
+        #resolution = 'HOUR' # MINUTE, MINUTE_2, MINUTE_3, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR, HOUR_2, HOUR_3, HOUR_4, DAY, WEEK, MONTH
+        resolution = 'H' # http://pandas.pydata.org/pandas-docs/stable/timeseries.html#dateoffset-objects
+        num_points = 10
+        response = ig_service.fetch_historical_prices_by_epic_and_num_points(epic, resolution, num_points)
+        print(response)
+        #print(response['prices']['price'])
+        #print(response['prices']['price']['ask'])
+        #print(response['prices']['volume'])
+        assert(isinstance(response['allowance'], dict))
+        #assert(isinstance(response['prices']['volume'], pd.Series))
+        #assert(isinstance(response['prices']['price'], pd.Panel))
+        assert(isinstance(response['prices'], pd.DataFrame))
+
+        print("")
+
+        print("fetch_historical_prices_by_epic_and_date_range")
+        start_date = datetime.datetime(2014, 12, 15)
+        end_date = datetime.datetime(2014, 12, 20)
+        response = ig_service.fetch_historical_prices_by_epic_and_date_range(epic, resolution, start_date, end_date)
+        print(response)
+        assert(isinstance(response['allowance'], dict))
+        #assert(isinstance(response['prices']['volume'], pd.Series))
+        #assert(isinstance(response['prices']['price'], pd.Panel))
+        assert(isinstance(response['prices'], pd.DataFrame))
