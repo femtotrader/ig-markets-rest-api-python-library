53ba7f2d0cdeabcae057e0a38cea7fc228aca8ec
femtotrader <femto.trader@gmail.com>
2015-01-28 11:52:12 +0100

    Minor change to sample

--- sample/sample.py
+++ sample/sample.py
@@ -13,53 +13,65 @@ logger.setLevel(logging.DEBUG)
 import datetime
 from datetime import timedelta
 import requests_cache
-expire_after = timedelta(hours=1)
-session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=expire_after)
-# set expire_after=None if you don't want cache expiration
-# set expire_after=0 if you don't want to cache queries
 
-#config = IGServiceConfig()
+def main():
+    expire_after = timedelta(hours=1)
+    session = requests_cache.CachedSession(cache_name='cache', backend='sqlite', expire_after=expire_after)
+    # set expire_after=None if you don't want cache expiration
+    # set expire_after=0 if you don't want to cache queries
 
-# no cache
-ig_service = IGService(config.username, config.password, config.api_key, config.acc_type)
+    #config = IGServiceConfig()
 
-# if you want to globally cache queries
-#ig_service = IGService(config.username, config.password, config.api_key, config.acc_type, session)
+    # no cache
+    ig_service = IGService(config.username, config.password, config.api_key, config.acc_type)
 
-ig_service.create_session()
+    # if you want to globally cache queries
+    #ig_service = IGService(config.username, config.password, config.api_key, config.acc_type, session)
 
-accounts = ig_service.fetch_accounts()
-print("accounts:\n%s" % accounts)
+    ig_service.create_session()
 
-#account_info = ig_service.switch_account(config.acc_number, False)
-#print(account_info)
+    accounts = ig_service.fetch_accounts()
+    print("accounts:\n%s" % accounts)
 
-open_positions = ig_service.fetch_open_positions()
-print("open_positions:\n%s" % open_positions)
+    #account_info = ig_service.switch_account(config.acc_number, False)
+    #print(account_info)
 
-print("")
+    #open_positions = ig_service.fetch_open_positions()
+    #print("open_positions:\n%s" % open_positions)
 
-working_orders = ig_service.fetch_working_orders()
-print("working_orders:\n%s" % working_orders)
+    print("")
 
-print("")
+    #working_orders = ig_service.fetch_working_orders()
+    #print("working_orders:\n%s" % working_orders)
 
-#epic = 'CS.D.EURUSD.MINI.IP'
-epic = 'IX.D.ASX.IFM.IP' # US (SPY) - mini
+    print("")
 
-resolution = 'D'
-# see from pandas.tseries.frequencies import to_offset
-# resolution = 'H'
-# resolution = '1Min'
+    #epic = 'CS.D.EURUSD.MINI.IP'
+    epic = 'IX.D.ASX.IFM.IP' # US (SPY) - mini
 
-num_points = 10
+    resolution = 'D'
+    # see from pandas.tseries.frequencies import to_offset
+    #resolution = 'H'
+    #resolution = '1Min'
 
-response = ig_service.fetch_historical_prices_by_epic_and_num_points(epic, resolution, num_points)
+    #num_points = 10
+    #response = ig_service.fetch_historical_prices_by_epic_and_num_points(epic, resolution, num_points)
 
-# if you want to cache this query
-#response = ig_service.fetch_historical_prices_by_epic_and_num_points(epic, resolution, num_points, session)
+    # if you want to cache this query
+    #response = ig_service.fetch_historical_prices_by_epic_and_num_points(epic, resolution, num_points, session)
 
-df_ask = response['prices']['ask']
-print("ask prices:\n%s" % df_ask)
+    #df_ask = response['prices']['ask']
+    #print("ask prices:\n%s" % df_ask)
 
-#print(trading_ig.__version__)
+    (start_date, end_date) = ('2015-01-15', '2015-01-28')
+    #response = ig_service.fetch_historical_prices_by_epic_and_date_range(epic, resolution, start_date, end_date)
+
+    # if you want to cache this query
+    response = ig_service.fetch_historical_prices_by_epic_and_date_range(epic, resolution, start_date, end_date, session)
+    df_ask = response['prices']['ask']
+    print("ask prices:\n%s" % df_ask)
+
+    #print(trading_ig.__version__)
+
+if __name__ == '__main__':
+    main()
